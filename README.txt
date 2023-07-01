0ac104cc9b757d3e5f5d6defee343d8b4d951d28
usmaniraghib <javaprogrammers786@gmail.com>
2023-07-01 16:39:16 +0530

    TestNgWithXml

---
+++ README.txt
@@ -0,0 +1,47 @@
+# SetUp Maven in Jenkins
+# Create Maven Project in Jenkins
+# Add Project Goal
+
+https://maven.apache.org/
+
+Apache Maven 3.6.3
+
+C:\apache-maven-3.6.3
+
+## Form Email Notification Settings:-
+
+STEP-1
+
+Manage Jenkins -> Configure System -> Email Notification
+
+SMTP (Simple Mail Transfer Protocol) Server : smtp.gmail.com
+
+Click on Advance button
+
+Click on "Use SMTP Authentication"
+User Name : raghibtest99@gmail.com
+Password : zaq1!QAZ
+Use SSL	: Click on this	
+SMTP Port : 465
+Reply-To Address : noreply@gmail.com
+Charset : UTF-8
+
+STEP-2
+
+Go to the Jobs -> Configure -> 
+
+Build Settings
+E-mail Notification	
+Recipients : raghibtest99@gmail.com
+
+Send e-mail for every unstable build
+
+Apply -> Save.
+
+## Schedule a job run :-
+
+Go to the Jobs -> Configure -> 
+
+Build Triggers -> Click on "Build periodically" ->
+
+50 * * * *
---
+++ src/test/java/com/raghib/testngwithxml/UserTest.java
@@ -0,0 +1,63 @@
+package com.raghib.testngwithxml;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+
+import org.testng.Assert;
+
+public class UserTest {
+	WebDriver webDriverObj = null;
+	WebDriverWait webDriverWaitObj = null;
+	WebElement webElementObj = null;
+	String expectedTitle = "Google";
+	
+  @Test
+  public void f() {
+	  String currentTitle = webDriverObj.getTitle();
+	  System.out.println("currentTitle : "+currentTitle);
+	  Assert.assertEquals(currentTitle, expectedTitle);
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+		// Chrome Browser
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\SELENIUM\\browser-driver\\chromedriver_win32\\chromedriver.exe");
+		webDriverObj = new ChromeDriver();
+		webDriverObj.manage().window().maximize();
+		webDriverObj.manage().deleteAllCookies();
+		
+		webDriverObj.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		webDriverObj.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		webDriverObj.get("https://www.google.com/");		
+		
+		//Refresh the browser.
+		webDriverObj.navigate().refresh();		 
+  }
+
+  @AfterTest
+  public void afterTest() {
+		try {
+			if (webDriverObj != null) {
+				System.out.println("Driver Need to Close");
+			} else {
+				System.out.println("Driver Still Open");
+			}
+		} catch (Exception e) {
+			System.out.println("Nothing to do with it");
+		} finally {
+			System.out.println("Finally Block - To close the driver");
+			webDriverObj.quit();
+		}
+  }
+
+}
