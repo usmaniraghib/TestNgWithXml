34bfda1bfb14c7192bbe2a19a5c3c25f90e76ffc
usmaniraghib <javaprogrammers786@gmail.com>
2023-08-06 13:55:48 +0530

    ok

--- src/test/java/com/raghib/testngwithxml/UserTest.java
+++ src/test/java/com/raghib/testngwithxml/UserTest.java
@@ -1,50 +1,49 @@
 package com.raghib.testngwithxml;
 
-import org.testng.annotations.Test;
-
-import org.testng.annotations.BeforeTest;
-
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UserTest {
 	WebDriver webDriverObj = null;
 	WebDriverWait webDriverWaitObj = null;
 	WebElement webElementObj = null;
 	String expectedTitle = "Google";
-	
-  @Test
-  public void f() {
-	  String currentTitle = webDriverObj.getTitle();
-	  System.out.println("currentTitle : "+currentTitle);
-	  Assert.assertEquals(currentTitle, expectedTitle);
-  }
-  
-  @BeforeTest
-  public void beforeTest() {
+
+	@Test
+	public void f() {
+		String currentTitle = webDriverObj.getTitle();
+		System.out.println("currentTitle : " + currentTitle);
+		Assert.assertEquals(currentTitle, expectedTitle);
+	}
+
+	@BeforeTest
+	public void beforeTest() {
 		// Chrome Browser
-	  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		webDriverObj = new ChromeDriver();
 		webDriverObj.manage().window().maximize();
 		webDriverObj.manage().deleteAllCookies();
-		
-		webDriverObj.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		webDriverObj.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		webDriverObj.get("https://www.google.com/");		
-		
-		//Refresh the browser.
-		webDriverObj.navigate().refresh();		 
-  }
 
-  @AfterTest
-  public void afterTest() {
+		webDriverObj.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+		webDriverObj.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		webDriverObj.get("https://www.google.com/");
+
+		// Refresh the browser.
+		webDriverObj.navigate().refresh();
+	}
+
+	@AfterTest
+	public void afterTest() {
 		try {
 			if (webDriverObj != null) {
 				System.out.println("Driver Need to Close");
@@ -57,6 +56,6 @@ public class UserTest {
 			System.out.println("Finally Block - To close the driver");
 			webDriverObj.quit();
 		}
-  }
+	}
 
 }
